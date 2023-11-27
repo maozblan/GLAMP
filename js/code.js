070c5c908887123b55fe7afa710b58e1e413bb6e
pmchoe <pmchoe@ucsc.edu>
2023-11-26 18:39:57 -0800

    TRIED adding dragging functionality to bus pass ONLY (emphasis on tried)

--- js/code.js
+++ js/code.js
@@ -2,3 +2,40 @@
 //Grace Galan, Maya Crolene, Philip Choe, Ashley Huynh, and Lyssa Li 
 //Art101 Fall Quarter 2023 with professor Wes Modes
 
+// 
+// initializing variables to track if something's being dragged, and offset too
+let currentlyDragging = false;
+let offsetX, offsetY;
+
+// gets bus pass img from html by its id
+const bus_pass = document.getElementById('bus-pass');
+
+// adds event listener on bus_pass when mouse is pressed down
+bus_pass.addEventListener('mousedown', (e) => {
+    // sets dragging state to true
+    currentlyDragging = true;
+
+    // calculates offset of mouse from top left corner of img
+    offsetX = e.clientX - bus_pass.getBoundingClientRect().left;
+    offsetY = e.clientY - bus_pass.getBoundingClientRect().top;
+});
+
+// adds event listener for dragging movement when mouse is moved
+document.addEventListener('mousemove', (e) => {
+    // check to see if img is currently being dragged
+    if(currentlyDragging) {
+        // calculates new img position based on mouse position
+        const x = e.clientX - offsetX;
+        const y = e.clientY - offsetY;
+
+        // changed img position to calcualted img position from above
+        bus_pass.style.left = `${x}px`;
+        bus_pass.style.top = `${y}px`;
+    }
+});
+
+// adds event listener to stop moving/dragging img when mouse is released
+document.addEventListener('mouseup', () => {
+    // sets dragging state to false when mouse is released
+    currentlyDragging = false;
+})
