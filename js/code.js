2c0c44573f1eb9e5ed333147e5b18569e7e24af3
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-11-29 16:12:38 -0800

    title page

--- js/code.js
+++ js/code.js
@@ -2,6 +2,11 @@
 //Grace Galan, Maya Crolene, Philip Choe, Ashley Huynh, and Lyssa Li 
 //Art101 Fall Quarter 2023 with professor Wes Modes
 
+// just making the title page disappear when clicked
+$("#title-page").click(function() {
+    $("#title-page").fadeOut(300);
+});
+
 // 
 // initializing variables to track if something's being dragged, and offset too
 let currentlyDragging = false;
