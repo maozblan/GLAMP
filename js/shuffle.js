c782944579ff0f41479001abc065e318a905c052
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-12-06 08:46:06 -0800

    fetch api

--- js/shuffle.js
+++ js/shuffle.js
@@ -3,59 +3,35 @@
 //Art101 Fall Quarter 2023 with professor Wes Modes
 
 let jsonData;
-// $(document).ready(function() {
-//     $.ajax({
-//         url: 'responses.json',   // path to json file
-//         dataType: 'json',
-//         success: function(data) {
-//         jsonData = data;
-//         console.log(jsonData);
-//         },
-//         error: function(jqXHR, textStatus, errorThrown) {
-//         console.error('Error loading JSON file:', textStatus, errorThrown);
-//         }
-//     });
-// });
-// ^^ just says the file can'e be found :(
+let validKeys;
+async function getResponses() {
+    let response = await fetch('./js/responses.json');
+    jsonData = await response.json();
+    
+    // pick out keys that have not been read
+    validKeys = Object.keys(jsonData);
 
-// literally writing in the json here for now :(
-jsonData = {
-    "1" : {
-        "text" : "\"incoming goodbye\ni used to worry and wonder a lot about how i'm going to remember you, but for now i've decided that i will just enjoy the time i have with you now. i don't know how it will be in the future, but i wish i could connect with the you from now again someday\""
-    },
-    "2" : {
-        "text" : "There was a girl that I had a crush on in junior high. She was super shy, but I could also tell she was really smart. I would put notes in her locker, but she would never talk to me. Then I moved away. Years later in high school I was visiting old Jr high friends and went to a party and there was a group of people surrounding this pretty girl who was talking animatedly. It was her! She had finally blossomed into a vivacious young woman! She saw me and called my name! But now it was my turn to be super shy and I just mumbled something and then avoided her for the rest of the party. Ever since then I've been wondering how she was, but she completely dropped off the map and no one knew where she was. Where did she go? What kind of person did she grow up into?"
-    }, 
-    "3" : {
-        "text" : "All these years I was just idly your friend, helping you look for the one. Yet sometimes I wonder if it's selfish to want you all to myself even when I know I cannot even give you the type of love that you look for."
-    },
-    "4" : {
-        "text" : "Three years ago I had a crush on my across the street neighbor. I observed him for a few weeks before we finally met. I was considering putting my number on a piece of paper and leaving it on his windshield, but luckily the universe had other plans. I ended up needing his help with a rogue homeless man who was harassing my apartment building at the time. We ended up dating and moved in together after 4 months. We are still together to this day 3 and a half years later."
-    },
-    "5" : {
-        "text" : "i fell in love with you while reading your favorite book. i think of you every time i see a magnificent tree, a tiny ceramic house, or a seahorse. you set a precedent i will never forget and you only loved me up close for 3 months."
-    },
-    "6" : {
-        "text" : "I got a crush on my friend in HS who was already in a relationship of 2 years. In our senior year, I slipped an anonymous love letter in his locker wishing him and his GF the best of luck in the future. He thought the letter was a prank."
-    },
-    "7" : {
-        "text" : "i once had a crush on one of my best friends in high school and im so glad i never told them. they turned out to be so controlling and passive agressive and im glad we dont talk anymore"
-    },
-};
-
-// corkboard shuffle implementaion
-$("#shuffle-button").click(function() {
-    // clear out the old post-it data
-    $('#corkboard .post-it').remove();
-    // randomize text selection from json
-    let keys = Object.keys(jsonData);
-    let pick = Math.floor(Math.random() * keys.length);
-    let text = jsonData[keys[pick]].text;
-    console.log(text);
-    // create div with with fetched text and append to corkboard
-    let div = $("<div>", {
-        class: "post-it",
-        text: text
+    // make corkboard clickable ONLY if json responses are fetched
+    // corkboard shuffle implementaion
+    $("#shuffle-button").click(function() {
+        // clear out the old post-it data
+        $('#corkboard .post-it').remove();
+        // randomize text selection from json
+        let pick = Math.floor(Math.random() * validKeys.length);
+        let text = jsonData[validKeys[pick]].text;
+        // remove key from valid keys
+        validKeys.splice(pick, 1);
+        // replenish valid keys
+        if (validKeys.length == 0) {
+            validKeys = Object.keys(jsonData);
+        }
+        console.log(text);
+        // create div with with fetched text and append to corkboard
+        let div = $("<div>", {
+            class: "post-it",
+            text: text
+        });
+        $("#corkboard").append(div);
     });
-    $("#corkboard").append(div);
-});
+}
+getResponses();     // call async function
