bc031ae7b358462c7e958c8f3c9c4c7b6416806d
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-12-10 23:08:25 -0800

    moved iframes to html

--- js/disk.js
+++ js/disk.js
@@ -4,14 +4,6 @@
 
 let p;
 
-let spotifyPlaylists = {
-    "grace" : '<iframe id="grace-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2Bex8YgZ74EobhnM1poSVp?utm_source=generator" width="100%" height="80px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-    "maya" : '<iframe id="maya-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1BeBLO59Ji3acFnaFpqbtZ?utm_source=generator" width="100%" height="80px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-    "philip" : '<iframe id="philip-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2TSOAUJOXI1n7HJulpRsaV?utm_source=generator" width="100%" height="80px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-    "ashley" : '<iframe id="ashley-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1OVGqSlxvE0d15aaTy8hGy?utm_source=generator" width="100%" height="80px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-    "lyssa" : '<iframe id="lyssa-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6V4FeoCo1qlBFI9LWyM7dI?utm_source=generator" width="100%" height="80px" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-}
-
 $(".disk").click(function() {
     p = $(this).data('p');
     // color palette change
@@ -36,9 +28,14 @@ $(".disk").click(function() {
     $(".disk-currently-playing").toggleClass("disk-currently-playing");
     $(this).toggleClass("disk-currently-playing");
 
+    // remove the starter text (only happens once)
+    $("#current-playlist-starter-text").hide();
+
     // embed the playlist and add <p> text
-    $(".current-playlist").html(spotifyPlaylists[p]);
     $(".current-playlist-text").html("<p>currently playing: " + p + "'s playlist</p>");
+    // to hide it within the loading screen
+    setTimeout(() => {
+        $(".current-playlist-embed").toggleClass("current-playlist-embed").hide();
+        $(`#${p}-iframe`).toggleClass("current-playlist-embed").show();
+    }, 160);
 });
-
-
